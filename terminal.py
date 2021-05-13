8cf0a17113ca88625ae500e1cb5cf85c4de2dc93
Wallbrothers <brothers@wallmarquez.com>
2021-05-13 09:18:28 -0400

    final run commit

---
+++ terminal.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+def root_set():
+  print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
+  root = input("run a file>>>")
+  if root == "exit":
+    sys.exit()
+
+  while root == "help":
+    f = open("ht.txt")
+    info = f.read()
+    print(info)
+    root = input("run a file>>>")
+
+  while root == "run":  
+    filename = input("filename>>>")
+    file = str("python3 " +  filename)
+    os.system(file)
+    print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
+    root = input("run a file>>>")
+    
+count = 0
+while count == 0:
+  root_set()
