9996119ebc4eea38eee2c6dc0caf850ed4972535
Wallbrothers <brothers@wallmarquez.com>
2021-05-14 09:57:07 -0400

    nevermind

--- terminal.py
+++ terminal.py
@@ -1,30 +1,5 @@
 import os
 import sys
 
-def root_set():
-  print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
-  root = input("run a file>>>")
-  if root == "exit":
-    sys.exit()
-
-  while root == "help":
-    f = open("ht.txt")
-    info = f.read()
-    print(info)
-    root = input("run a file>>>")
-
-  while root == "run":  
-    filename = input("filename>>>")
-    file = str("python3 " +  filename)
-    os.system(file)
-    #print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
-    root = input("run a file>>>")
-  if root == "cls":
-    os.system("cls")
-  if root == "cd":
-    foldername = input("folder>>>")
-    os.system("cd " + foldername)
-    
-count = 0
-while count == 0:
-  root_set()
+while __name__ == "__main__":
+  pass 
