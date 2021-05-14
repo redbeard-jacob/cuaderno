6ed8e05e10b2d0bced85e5db5fa2b963278fe5d6
Wallbrothers <brothers@wallmarquez.com>
2021-05-14 13:10:24 -0400

    re set

--- terminal.py
+++ terminal.py
@@ -1,5 +1,26 @@
 import os
 import sys
 
-while __name__ == "__main__":
-  pass 
+def root_set():
+  print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
+  root = input("run a file>>>")
+  if root == "exit":
+    sys.exit()
+
+  while root == "help":
+    f = open("ht.txt")
+    info = f.read()
+    print(info)
+    print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
+    root = input("run a file>>>")
+
+  while root == "run":  
+    filename = input("filename>>>")
+    file = str("python3 " +  filename)
+    os.system(file)
+    print("type 'help' for more options and 'exit' to exit and 'run' to run a file")
+    root = input("run a file>>>")
+    
+count = 0
+while count == 0:
+  root_set()
